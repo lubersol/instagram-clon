7ec2afdfdda71d3636810222d618d819d63372d2
Lucia <lubersol@gmail.com>
2021-05-11 19:07:42 +0200

    using hooks in Login, installing tailwind

---
+++ src/pages/login.js
@@ -0,0 +1,26 @@
+import { useState, useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
+
+export default function Login() {
+    const history = useHistory();
+    const { firebase } = useContext(FirebaseContext);
+
+    const [ emailAddress,setEmailAddress] = useState('');
+    const [ password, setPassword]= useState('');
+
+    const [error, setError]= useState('');
+    const isInvalid = password ==='' || emailAddress==='';
+
+    const handleLogin = () => {};
+
+useEffect(() => {
+    document.title='Login - Instagram';
+}, []);
+
+    return(
+<div className="comtainer flex mx-auto max-w-screen-md items-center h-screen">
+    <p>I have no idea!</p>
+</div>
+    );
+}
